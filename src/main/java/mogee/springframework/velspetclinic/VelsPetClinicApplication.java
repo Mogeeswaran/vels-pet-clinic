80466fd2eb5fdb9fca9baffc09124c9f2e75736a
Mogeeswaran <mogeeswaran1993@gmai.com>
2020-09-26 20:36:30 +0530

    Initial Project commit

---
+++ src/main/java/mogee/springframework/velspetclinic/VelsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mogee.springframework.velspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelsPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mogee/springframework/velspetclinic/VelsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mogee.springframework.velspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelsPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
