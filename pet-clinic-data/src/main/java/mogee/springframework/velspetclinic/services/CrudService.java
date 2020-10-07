9e50fe47278d6d4e4409e7158fa434f58b99cd92
Mogeeswaran <mogeeswaran1993@gmai.com>
2020-10-07 18:07:33 +0530

    Impl Map based Services. closes #6

--- pet-clinic-data/src/main/java/mogee/springframework/velspetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/mogee/springframework/velspetclinic/services/CrudService.java
@@ -7,7 +7,7 @@ public interface CrudService<T,ID> {
     Set<T> findAll();
     T findById(ID id);
     T save(T object);
-    T delete(T object);
-    T deleteById(ID id);
+    void delete(T object);
+    void deleteById(ID id);
 
 }
---
+++ pet-clinic-data/src/main/java/mogee/springframework/velspetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package mogee.springframework.velspetclinic.services.map;
+
+import mogee.springframework.velspetclinic.model.Pet;
+import mogee.springframework.velspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
